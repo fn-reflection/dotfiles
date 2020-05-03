125e21f4c5f7969b287def4e2b043e47f59a6b03
Naoki Fujita <fujitanaoki0718@gmail.com>
2020-05-03 14:46:52 +0900

    add update_env.sh

---
+++ update_env.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function force_update_sym_link() {
+    SCRIPT_DIR=$(pwd)
+    path=$1
+    ln -sf $SCRIPT_DIR/$path ~/$path
+}
+
+cd $(dirname $0)
+git submodule update --init --recursive #initlize external repository
+
+##prezto
+force_update_sym_link .zprezto
+force_update_sym_link .zpreztorc
+##zsh
+force_update_sym_link .zprofile
+force_update_sym_link .zshrc
+
+force_update_sym_link .vimrc
+force_update_sym_link .tmux.conf
