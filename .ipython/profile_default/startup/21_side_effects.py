39fdb789ce8eb4e5b50a78f10e0b7a3e3ec787ea
Naoki Fujita <fujitanaoki0718@gmail.com>
2021-01-04 06:50:16 +0900

    fix: syntax error

--- .ipython/profile_default/startup/21_side_effects.py
+++ .ipython/profile_default/startup/21_side_effects.py
@@ -2,9 +2,9 @@ np.set_printoptions(edgeitems=30, linewidth=100000,
                     formatter=dict(float=lambda x: "%.7g" % x))
 try:
     lenv.ipython.register_event(10)
-NameError:
+except NameError:
     pass
 try:
     lactivemodel.PG_10_10_10_4_M.bind(lactivemodel.MODELS)
-NameError:
+except NameError:
     pass
