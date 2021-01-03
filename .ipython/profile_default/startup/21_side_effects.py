f00dde9d2ae68326a6991bcc6d471037f60d1311
Naoki Fujita <fujitanaoki0718@gmail.com>
2021-01-04 06:49:36 +0900

    feat: エラーでスクリプトが止まるのを防止する

--- .ipython/profile_default/startup/21_side_effects.py
+++ .ipython/profile_default/startup/21_side_effects.py
@@ -1,3 +1,10 @@
-lenv.ipython.register_event(10)
-np.set_printoptions(edgeitems=30, linewidth=100000, formatter=dict(float=lambda x: "%.7g" % x))
-lactivemodel.PG_10_10_10_4_M.bind(lactivemodel.MODELS)
+np.set_printoptions(edgeitems=30, linewidth=100000,
+                    formatter=dict(float=lambda x: "%.7g" % x))
+try:
+    lenv.ipython.register_event(10)
+NameError:
+    pass
+try:
+    lactivemodel.PG_10_10_10_4_M.bind(lactivemodel.MODELS)
+NameError:
+    pass
