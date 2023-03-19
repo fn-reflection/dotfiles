52438b325474a9cd25f31c24baf1d3a5eba08254
Naoki Fujita <fujitanaoki0718@gmail.com>
2023-03-19 22:02:23 +0900

    feat: Cicaを入れる

--- .wezterm.lua
+++ .wezterm.lua
@@ -6,7 +6,7 @@ return {
     cursor_blink_rate = 500,
     default_cursor_style = 'BlinkingUnderline',
     disable_default_key_bindings = true,
-    font = wezterm.font_with_fallback({'FiraCode Nerd Font', 'JetBrains Mono'}),
+    font = wezterm.font_with_fallback({'FiraCode Nerd Font', 'Cica'}),
     font_size = 16.0,
     keys = {
         {key = 'c', mods = 'CTRL', action = wezterm.action {CopyTo = "Clipboard"}}, -- CTRL+C for SIGINT
