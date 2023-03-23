8d55686b2a57d59ece82aaaff4eaf41c2b5df5a3
Naoki Fujita <fujitanaoki0718@gmail.com>
2023-03-24 00:26:26 +0900

    feat: add neovim file

--- update_env.sh
+++ update_env.sh
@@ -1,44 +1,47 @@
 #!/bin/bash
 
 function force_update_sym_link() {
-    SCRIPT_DIR=$(pwd)
-    path=$1
-    ln -sf $SCRIPT_DIR/$path ~/$path
+	SCRIPT_DIR=$(pwd)
+	path=$1
+	ln -sf $SCRIPT_DIR/$path ~/$path
 }
 
 function update_sym_link() {
-    SCRIPT_DIR=$(pwd)
-    path=$1
-    if [ ! -e ~/$path ]; then
-      force_update_sym_link $path
-    fi
+	SCRIPT_DIR=$(pwd)
+	path=$1
+	if [ ! -e ~/$path ]; then
+		force_update_sym_link $path
+	fi
 }
 
 cd $(dirname $0)
 
-##zsh
+# zsh
 force_update_sym_link .zprofile
 force_update_sym_link .zshrc
 
-#wezterm
+# wezterm
 force_update_sym_link .wezterm.lua
 
-#vim
+# vim
 force_update_sym_link .vimrc
 update_sym_link .vim/colors
 
-##git
+# nvim
+force_update_sym_link .config/nvim/init.lua
+
+# git
 force_update_sym_link .gitconfig
 
-##tmux
+# tmux
 force_update_sym_link .tmux.conf
 
-##python
+# python
 update_sym_link .ipython/profile_default/startup
 update_sym_link .vim/colors
 force_update_sym_link .jupyter/jupyter_notebook_config.py
 force_update_sym_link .config/pep8
 force_update_sym_link .config/pylintrc
 
-##ruby
+# ruby
 force_update_sym_link .pryrc
