4bdf5de4d07151bf5f77c5bace4bbd83aff4d3d3
Naoki Fujita <fujitanaoki0718@gmail.com>
2023-03-25 21:23:17 +0900

    feat: 一旦コミット

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,59 @@
-vim.wo.number = true;
+--[[
+vim.g.did_install_default_menus = 1;
+vim.g.did_install_syntax_menu = 1;
+vim.g.did_indent_on = 1;
+vim.g.did_load_filetypes = 1;
+vim.g.did_load_ftplugin = 1;
+vim.g.loaded_2html_plugin = 1;
+vim.g.loaded_gzip = 1;
+vim.g.loaded_man = 1;
+vim.g.loaded_matchit = 1;
+vim.g.loaded_matchparen = 1;
+vim.g.loaded_netrwPlugin = 1;
+vim.g.loaded_remote_plugins = 1;
+vim.g.loaded_shada_plugin = 1;
+vim.g.loaded_spellfile_plugin = 1;
+vim.g.loaded_tarPlugin = 1;
+vim.g.loaded_tutor_mode_plugin = 1;
+vim.g.loaded_zipPlugin = 1;
+vim.g.skip_loading_mswin = 1;
+vim.g.skip_defaults_vim = 1;
+--]] -- KEY MAPPING
+vim.g.mapleader = ' '
+vim.g.whichwrap = '<,>,h,l'
+vim.keymap.set('i', 'jj', '<esc>')
+
+-- CUSTOMIZE
+vim.opt.clipboard:append{'unnamedplus'} -- use system clipboard
+vim.opt.ignorecase = true
+vim.opt_local.number = true -- show line numbers
+vim.opt.mouse = a -- enable mouse on all modes
+vim.opt.smartcase = true -- case sensitive search when query has uppercase character
+--[[
+vim.cmd('packadd vim-jetpack')
+require('jetpack.packer').startup(function(use)
+    -- use from VSCode NeoVim or NeoVim
+    use 'tpope/vim-commentary'
+    use 'easymotion/vim-easymotion'
+    use 'haya14busa/vim-edgemotion'
+
+    if vim.g.vscode then
+        return nil
+    end
+
+    -- use from only NeoVim
+    use 'feline-nvim/feline.nvim'
+    use 'zefei/vim-wintabs'
+    use {
+        'neoclide/coc.nvim',
+        branch = 'release'
+    }
+    use 'lambdalisue/fern.vim'
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        ['do'] = ':TSUpdate'
+    }
+    use 'airblade/vim-gitgutter'
+    use 'simeji/winresizer'
+end)
+--]]
