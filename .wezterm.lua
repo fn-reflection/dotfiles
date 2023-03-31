600cbc607e5eced41417f1cf274d0a94bdb3f717
Naoki Fujita <fujitanaoki0718@gmail.com>
2023-03-31 15:12:32 +0900

    feat: 3日間の天気予報が出るようにした

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,12 @@
 local wezterm = require 'wezterm'
 local SOLID_LEFT_ARROW = utf8.char(0xe0b2)
 
-function enumerate(tbl, func)
+wezterm.GLOBAL.weather_loop_counter = 0
+wezterm.GLOBAL.weathercodes = {-1, -1, -1}
+wezterm.GLOBAL.min_temperatures = {'?', '?', '?'}
+wezterm.GLOBAL.max_temperatures = {'?', '?', '?'}
+
+function enumerate(tbl, func) -- almost same as python's enumerate(map with index)
     local t = {}
     for idx, v in ipairs(tbl) do
         t[idx] = func(v, idx)
@@ -9,7 +14,28 @@ function enumerate(tbl, func)
     return t
 end
 
-function flatten1(tbls)
+-- https://gist.github.com/w13b3/5d8a80fae57ab9d51e285f909e2862e0
+function zip(...) -- almost same as python's zip
+    local idx, ret, args = 1, {}, {...}
+    while true do -- loop smallest table-times
+        local sub_table = {}
+        for _, table_ in ipairs(args) do
+            value = table_[idx] -- becomes nil if index is out of range
+            if value == nil then
+                break
+            end -- break for-loop
+            table.insert(sub_table, value)
+        end
+        if value == nil then
+            break
+        end -- break while-loop
+        table.insert(ret, sub_table) -- insert the sub result
+        idx = idx + 1
+    end
+    return ret
+end
+
+function flatten1(tbls) -- flatten 1 level
     local result = {}
     for index, tbl in next, tbls do
         for _, value in next, tbl do
@@ -24,7 +50,71 @@ function day_of_week_in_japan(weeknum)
     return days[weeknum + 1]
 end
 
-function powerline(text, color)
+-- https://open-meteo.com/en/docs
+function weather_symbol(weathercode)
+    local weathers = {
+        [-1] = '❓', -- undefined
+        [0] = '☀️',
+        [1] = '🌤',
+        [2] = '🌦',
+        [3] = '☁️',
+        [45] = '🌫',
+        [48] = '🌫',
+        [51] = '🌦',
+        [53] = '🌦',
+        [55] = '🌧',
+        [56] = '🌨',
+        [57] = '🌨',
+        [61] = '☂',
+        [63] = '☔',
+        [65] = '☔',
+        [66] = '☂',
+        [67] = '☔',
+        [71] = '⛄',
+        [73] = '☃',
+        [75] = '☃',
+        [77] = '🌨',
+        [80] = '🌧',
+        [81] = '☂',
+        [82] = '☔',
+        [85] = '🌨',
+        [86] = '🌨',
+        [95] = '⛈',
+        [96] = '⛈',
+        [99] = '⛈'
+    }
+    return weathers[weathercode]
+end
+
+function styled_whether(weathercode, temperature_max, temperature_min)
+    return {{
+        Foreground = {
+            Color = "#c0c0c0"
+        }
+    }, {
+        Text = string.format(" %s", weather_symbol(weathercode))
+    }, {
+        Foreground = {
+            Color = "#f08300" -- hot color
+        }
+    }, {
+        Text = string.format("%s", temperature_max)
+    }, {
+        Foreground = {
+            Color = "#c0c0c0"
+        }
+    }, {
+        Text = '|'
+    }, {
+        Foreground = {
+            Color = "#89c3eb" -- cool color
+        }
+    }, {
+        Text = string.format("%s ", temperature_min)
+    }}
+end
+
+function powerline(styled_text, color) -- decorate right prompt
     return {{
         Foreground = {
             Color = color
@@ -35,28 +125,53 @@ function powerline(text, color)
         Background = {
             Color = color
         }
-    }, {
+    }, table.unpack(styled_text)}
+end
+
+function update_whether()
+    local success, stdout, stderr = wezterm.run_child_process {'curl', '--silent',
+                                                               'https://api.open-meteo.com/v1/forecast?latitude=35.7&longitude=139.82&daily=weathercode,temperature_2m_max,temperature_2m_min&forecast_days=3&timezone=Asia%2FTokyo'} -- At Tokyo SkyTree
+    if not (success) or not (stdout) then
+        return
+    end
+    local res = wezterm.json_parse(stdout).daily
+    wezterm.GLOBAL.weathercodes = res.weathercode
+    wezterm.GLOBAL.min_temperatures = res.temperature_2m_min
+    wezterm.GLOBAL.max_temperatures = res.temperature_2m_max
+end
+
+function create_powerlines(window, pane)
+    local weather_infos = zip(wezterm.GLOBAL.weathercodes, wezterm.GLOBAL.max_temperatures,
+        wezterm.GLOBAL.min_temperatures)
+    local styled_whethers = enumerate(weather_infos, function(weather_info, index)
+        return styled_whether(table.unpack(weather_info))
+    end)
+    local styled_texts = {styled_whethers[1], styled_whethers[2], styled_whethers[3], {{
         Foreground = {
             Color = "#c0c0c0"
         }
     }, {
-        Text = string.format(" %s ", text)
-    }}
-end
-
-function update_right_status(window, pane)
-    local texts = {(pane:get_current_working_dir() or ""):sub(8),
-                   string.format("%s(%s) %s", wezterm.strftime("%-m/%-d"), day_of_week_in_japan(wezterm.strftime("%u")),
-        wezterm.strftime("%H:%M:%S"))};
-    local powerlines = flatten1(enumerate(texts, function(text, index)
-        local colors = {"#3c1361", "#52307c", "#663a82", "#7c5295", "#b491c8"}
-        return powerline(text, colors[index])
+        Text = string.format(" %s ", (pane:get_current_working_dir() or ""):sub(8))
+    }}, {{
+        Foreground = {
+            Color = "#c0c0c0"
+        }
+    }, {
+        Text = string.format(" %s(%s) %s ", wezterm.strftime("%-m/%-d"), day_of_week_in_japan(wezterm.strftime("%u")),
+            wezterm.strftime("%H:%M:%S"))
+    }}};
+    return flatten1(enumerate(styled_texts, function(styled_text, index)
+        local color = string.format("hsl(%sdeg 75%% 25%%)", index % 2 == 0 and 240 or 224)
+        return powerline(styled_text, color)
     end));
-    window:set_right_status(wezterm.format(powerlines))
 end
 
-wezterm.on("update-right-status", function(window, pane)
-    update_right_status(window, pane)
+wezterm.on("update-status", function(window, pane)
+    window:set_right_status(wezterm.format(create_powerlines(window, pane)))
+    if wezterm.GLOBAL.weather_loop_counter % (3600 * 4) == 0 then -- every 4 hours
+        update_whether()
+    end
+    wezterm.GLOBAL.weather_loop_counter = wezterm.GLOBAL.weather_loop_counter + 1;
 end);
 
 return {
@@ -253,5 +368,5 @@ return {
     tab_max_width = 24,
     use_dead_keys = false,
     window_decorations = 'RESIZE', -- no title bar
-    window_background_opacity = 0.75
+    window_background_opacity = 0.7
 }
